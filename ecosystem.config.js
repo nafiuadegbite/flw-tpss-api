8ffb0eecd2b3cd25e6a612ca4a87d54aced6efb6
nafiu adegbite <youngguru23@gmail.com>
2022-07-04 15:57:46 +0100

    Setting up heroku

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "flw-tpss-api",
+      script: "./server.js",
+      instances: "max",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
