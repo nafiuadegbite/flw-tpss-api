d28713d79fa2724386098ec2720a9ee759c5e612
nafiu adegbite <youngguru23@gmail.com>
2022-07-04 15:44:32 +0100

    Setting up heroku

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const morgan = require("morgan");
 
 const { tpssRouter } = require("./routes/tpss.router");
 
 const app = express();
 
 app.use(cors());
-app.use(morgan("combined"));
 app.use(express.json());
 
 app.use("/split-payments/compute", tpssRouter);
